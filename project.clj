50b00c8efddd9ddb9aba5b4c510c0d7c16bb9b11
Lucas Reis <lucasmreis@gmail.com>
2015-06-17 09:56:40 -0300

    environ and time

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.forward/clojure-mail "1.0"]
-                 [environ "1.0.0"]])
+                 [environ "1.0.0"]
+                 [clj-time "0.9.0"]])
--- src/ourdaily/core.clj
+++ src/ourdaily/core.clj
@@ -1,9 +1,22 @@
 (ns ourdaily.core
   (:require [clojure-mail.core :refer :all]
             [clojure-mail.message :as message]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clj-time.format :as f]))
 
 (def gmail-username (env :gmail-username))
 (def gmail-password (env :gmail-password))
 
-(def my-store (gen-store gmail-username gmail-password))
+(def ourdaily-store (gen-store gmail-username gmail-password))
+
+(def gmail-date-formatter (f/formatter "E MMM d H:m:s z y"))
+
+(defn by-time
+  [from to]
+  (fn [msg]
+    (and (> (:date-sent msg) from)
+         (< (:date-sent msg) to))))
+
+(defn get-messages
+  [from to max-messages]
+  filter)
